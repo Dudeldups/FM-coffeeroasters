177f7032118cdf84caaac48b2a793ec623eafdf3
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-02-22 00:01:41 +0100

    added footer styling + white logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
-import coffeeroastersLogo from "../assets/shared/desktop/logo.svg";
+import coffeeroastersLogoWhite from "../assets/shared/desktop/logo-white.svg";
 import Facebook from "./logos/Facebook";
 import Instagram from "./logos/Instagram";
 import Twitter from "./logos/Twitter";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <img src={coffeeroastersLogo} alt="Coffeeroasters logo" />
+    <footer className="mb-[40rem] flex justify-center px-6 sm:px-10">
+      <div className="flex w-full max-w-7xl flex-col items-center bg-custom-dark-blue px-5 py-12 lg:flex-row lg:items-center lg:justify-between lg:px-12 xl:px-20">
+        <img
+          className="w-52 sm:w-60"
+          src={coffeeroastersLogoWhite}
+          alt="Coffeeroasters logo"
+        />
 
-        <nav>
-          <ul>
+        <nav className="mt-10 md:mt-8 lg:mt-0">
+          <ul className="text-custom-light-grey flex flex-col items-center gap-3 font-bold md:flex-row md:gap-8">
             <li>
               <Link to="/">Home</Link>
             </li>
@@ -24,7 +28,7 @@ const Footer = () => {
           </ul>
         </nav>
 
-        <ul>
+        <ul className="mt-10 flex items-center justify-center gap-6 md:mt-14 lg:mt-0">
           <li>
             <Facebook />
           </li>
