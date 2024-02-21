72db9386b3f2b8233f501f25fcc5c039e63a6aa3
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2024-02-21 13:29:10 +0100

    added fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          "dark-cyan": "#0E8784",
+          "dark-blue": "#333D4B",
+          "pale-orange": "#FDD6BA",
+          "light-cream": "#FEFCF7",
+          grey: "#83888F",
+        },
+      },
+      fontFamily: {
+        barlow: ["Barlow", "sans-serif"],
+        fraunces: ["Faunces", "serif"],
+      },
+    },
   },
   plugins: [],
 };
