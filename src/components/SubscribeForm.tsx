f2d1557ac0919f269bbc8730d36b68a7db84db64
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-02-26 00:00:20 +0100

    added base form styling

--- src/components/SubscribeForm.tsx
+++ src/components/SubscribeForm.tsx
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
+import subscriptionData from "../data/subscriptions.json";
+import Btn from "./Btn";
+
 interface SubscribeFormProps {
   summaryString: string;
   setSummaryString: React.Dispatch<React.SetStateAction<string>>;
@@ -8,9 +11,9 @@ interface SubscribeFormProps {
 }
 interface Prices {
   [key: string]: {
-    everyWeek: number;
-    everyTwoWeeks: number;
-    everyMonth: number;
+    Everyweek: number;
+    Every2weeks: number;
+    Everymonth: number;
   };
 }
 
@@ -23,7 +26,10 @@ const SubscribeForm = ({
     register,
     handleSubmit,
     watch,
-    formState: { isSubmitting },
+    formState: { errors, isSubmitting },
+    setError,
+    clearErrors,
+    setValue,
   } = useForm<FieldValues>();
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
@@ -34,373 +40,161 @@ const SubscribeForm = ({
 
   const prices: Prices = {
     "250g": {
-      everyWeek: 7.2,
-      everyTwoWeeks: 9.6,
-      everyMonth: 12.0,
+      Everyweek: 7.2,
+      Every2weeks: 9.6,
+      Everymonth: 12.0,
     },
     "500g": {
-      everyWeek: 13.0,
-      everyTwoWeeks: 17.5,
-      everyMonth: 22.0,
+      Everyweek: 13.0,
+      Every2weeks: 17.5,
+      Everymonth: 22.0,
     },
     "1000g": {
-      everyWeek: 22.0,
-      everyTwoWeeks: 32.0,
-      everyMonth: 42.0,
+      Everyweek: 22.0,
+      Every2weeks: 32.0,
+      Everymonth: 42.0,
     },
   };
 
-  const className = {
-    fieldset: "my-6",
-    input: "sr-only peer",
-    label: "block peer-checked:bg-slate-300",
-    "label-span": "block font-bold",
-    "inner-div": ``,
-  };
+  console.log(errors);
 
-  const quantity = watch("quantity") as keyof Prices;
+  const formData = watch();
 
-  const isCapsuleSelected = watch("preferences") === "Capsule";
-  const isPreferencesSelected =
-    watch("preferences") !== undefined && watch("preferences") !== null;
-  const isBeanTypeSelected =
-    watch("beanType") !== undefined && watch("beanType") !== null;
-  const isQuantitySelected =
-    watch("quantity") !== undefined && watch("quantity") !== null;
-  const isGrindOptionSelected =
-    watch("grindOption") !== undefined && watch("grindOption") !== null;
-  const isDeliveriesSelected =
-    watch("deliveries") !== undefined && watch("deliveries") !== null;
-
-  const isEverythingForDeliveriesSelected =
-    (isCapsuleSelected && isQuantitySelected) || isGrindOptionSelected;
+  const currentFormStep =
+    Object.values(formData).filter(
+      (value) => value !== null && value !== undefined,
+    ).length + 1;
+  const isCapsuleSelected = formData["Preferences"] === "Capsule";
 
   useEffect(() => {
     setSummaryString(
       `"I drink my coffee ${
         isCapsuleSelected
           ? "using Capsules"
-          : `as ${watch("preferences") || "_____"}`
-      }, with a ${watch("beanType") || "_____"} type of bean. ${
-        watch("quantity") || "_____"
-      }${
+          : `as ${formData["Preferences"] || "_____"}`
+      }, with a ${formData["Bean-Type"] || "_____"} type of bean. ${formData["Quantity"] || "_____"}${
         !isCapsuleSelected
-          ? watch("grindOption")
-            ? ` ground ala ${watch("grindOption")}`
+          ? formData["Grind-Option"]
+            ? ` ground ala ${formData["Grind-Option"]}`
             : " ground ala _____"
           : ""
-      }, sent to me ${watch("deliveries") || "_____"}.`,
+      }, sent to me ${formData["Deliveries"] || "_____"}.`,
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    isPreferencesSelected,
-    isBeanTypeSelected,
-    isQuantitySelected,
-    isGrindOptionSelected,
-    isDeliveriesSelected,
-  ]);
+  }, [formData]);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <fieldset className={className.fieldset}>
-        <legend className="peer">How do you drink your coffee?</legend>
-
-        <div className={className["inner-div"]}>
-          <input
-            className={className.input}
-            {...register("preferences", { required: true })}
-            type="radio"
-            id="preferences-capsule"
-            value="Capsule"
-          />
-          <label className={className.label} htmlFor="preferences-capsule">
-            <span className={className["label-span"]}>Capsule</span> Compatible
-            with Nespresso systems and similar brewers
-          </label>
-        </div>
-
-        <div className={className["inner-div"]}>
-          <input
-            className={className.input}
-            {...register("preferences", { required: true })}
-            type="radio"
-            id="preferences-filter"
-            value="Filter"
-          />
-          <label className={className.label} htmlFor="preferences-filter">
-            <span className={className["label-span"]}>Filter</span> For pour
-            over or drip methods like Aeropress, Chemex, and V60
-          </label>
-        </div>
-
-        <div className={className["inner-div"]}>
-          <input
-            className={className.input}
-            {...register("preferences", { required: true })}
-            type="radio"
-            id="preferences-espresso"
-            value="Espresso"
-          />
-          <label className={className.label} htmlFor="preferences-espresso">
-            <span className={className["label-span"]}>Espresso</span> Dense and
-            finely ground beans for an intense, flavorful experience
-          </label>
-        </div>
-      </fieldset>
-
-      <fieldset
-        className={className.fieldset}
-        aria-expanded={isPreferencesSelected}
-        aria-live="polite"
-      >
-        <legend>What type of coffee?</legend>
-        {isPreferencesSelected && (
-          <div>
-            <div className={className["inner-div"]}>
-              <input
-                className={className.input}
-                {...register("beanType", { required: true })}
-                type="radio"
-                id="beanType-singleOrigin"
-                value="Single Origin"
-              />
-              <label
-                className={className.label}
-                htmlFor="beanType-singleOrigin"
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="mt-28 md:mt-36 md:max-w-[45rem] lg:mt-40"
+    >
+      {currentFormStep}
+      {`${formData["Preferences"]}`}
+      {`${formData["Bean-Type"]}`}
+      <ul className="grid gap-24 lg:gap-20">
+        {subscriptionData.map((question) => {
+          const isQuestionFive = question.id === 5;
+          const questionSplit = question.name.split(" ").join("-");
+          return (
+            <li key={question.id + question.question}>
+              <fieldset
+                className=""
+                aria-expanded={currentFormStep === question.id}
+                aria-live="polite"
               >
-                <span className={className["label-span"]}>Single Origin</span>{" "}
-                Distinct, high quality coffee from a specific family-owned farm
-              </label>
-            </div>
+                <legend className="peer flex w-full items-start justify-between gap-4">
+                  <h2 className="text-lg/[1.75rem] text-custom-dark-grey md:text-xl lg:text-2xl">
+                    {question.question}
+                  </h2>
 
-            <div className={className["inner-div"]}>
-              <input
-                className={className.input}
-                {...register("beanType", { required: true })}
-                type="radio"
-                id="beanType-decaf"
-                value="Decaf"
-              />
-              <label className={className.label} htmlFor="beanType-decaf">
-                <span className={className["label-span"]}>Decaf</span> Just like
-                regular coffee, except the caffeine has been removed
-              </label>
-            </div>
+                  <button type="button" className="mt-3 inline w-6">
+                    <span className="sr-only">Expand</span>
+                    <span className="mx-auto block h-3 w-3 -rotate-45 border-r-[3px] border-t-[3px] border-custom-dark-cyan"></span>
+                  </button>
+                </legend>
 
-            <div className={className["inner-div"]}>
-              <input
-                className={className.input}
-                {...register("beanType", { required: true })}
-                type="radio"
-                id="beanType-blended"
-                value="Blended"
-              />
-              <label className={className.label} htmlFor="beanType-blended">
-                <span className={className["label-span"]}>Blended</span>{" "}
-                Combination of two or three dark roasted beans of organic
-                coffees
-              </label>
-            </div>
-          </div>
-        )}
-      </fieldset>
+                <div className="mt-8 grid gap-4 md:mt-10 md:grid-cols-3 md:gap-2 lg:mt-14 lg:gap-6">
+                  {question.options.map((option) => {
+                    const optionSplit = option.name.split(" ").join("-");
+                    return (
+                      <div
+                        key={question.id + option.name}
+                        className={`cursor-pointer rounded-lg p-6 outline-2 outline-offset-2 outline-custom-dark-grey focus-within:bg-custom-pale-orange focus-within:outline ${formData[questionSplit] === option.name ? "bg-custom-dark-cyan text-custom-light-cream" : "bg-custom-very-light-grey text-custom-dark-blue hover:bg-custom-pale-orange"}`}
+                        onClick={() => {
+                          if (currentFormStep < question.id) {
+                            console.log("true");
+                            setError(questionSplit, {
+                              message:
+                                "Please answer the previous question first.",
+                            });
+                          } else setValue(questionSplit, option.name);
+                        }}
+                      >
+                        <input
+                          className="sr-only"
+                          {...register(questionSplit, {
+                            required:
+                              currentFormStep !== 4 || !isCapsuleSelected,
+                            onChange: () => clearErrors(),
+                            disabled: currentFormStep < question.id,
+                          })}
+                          type="radio"
+                          id={`${questionSplit}-${optionSplit}`}
+                          value={option.name}
+                          aria-describedby={`${questionSplit}-${optionSplit}-d`}
+                        />
+                        <label
+                          className={`cursor-pointer ${currentFormStep >= question.id ? "" : ""}`}
+                          htmlFor={`${questionSplit}-${optionSplit}`}
+                          onClick={() => {
+                            if (currentFormStep < question.id) {
+                              console.log("true");
+                              setError(questionSplit, {
+                                message:
+                                  "Please answer the previous question first.",
+                              });
+                            }
+                          }}
+                        >
+                          <span className={`font-fraunces text-lg font-black`}>
+                            {option.name}
+                          </span>
+                        </label>
 
-      <fieldset
-        className={className.fieldset}
-        aria-expanded={isBeanTypeSelected}
-        aria-live="polite"
-      >
-        <legend>How much would you like?</legend>
-        {isBeanTypeSelected && (
-          <div>
-            <div>
-              <input
-                className={className.input}
-                {...register("quantity", { required: true })}
-                type="radio"
-                id="quantity-250g"
-                value="250g"
-              />
-              <label className={className.label} htmlFor="quantity-250g">
-                <span className={className["label-span"]}>250g</span> Perfect
-                for the solo drinker. Yields about 12 delicious cups.
-              </label>
-            </div>
-
-            <div>
-              <input
-                className={className.input}
-                {...register("quantity", { required: true })}
-                type="radio"
-                id="quantity-500g"
-                value="500g"
-              />
-              <label className={className.label} htmlFor="quantity-500g">
-                <span className={className["label-span"]}>500g</span> Perfect
-                option for a couple. Yields about 40 delectable cups.
-              </label>
-            </div>
-
-            <div>
-              <input
-                className={className.input}
-                {...register("quantity", { required: true })}
-                type="radio"
-                id="quantity-1000g"
-                value="1000g"
-              />
-              <label className={className.label} htmlFor="quantity-1000g">
-                <span className={className["label-span"]}>1000g</span> Perfect
-                for offices and events. Yields about 90 delightful cups.
-              </label>
-            </div>
-          </div>
-        )}
-      </fieldset>
-
-      <fieldset
-        className={className.fieldset}
-        aria-expanded={isQuantitySelected}
-        aria-live="polite"
-      >
-        <legend>Want us to grind them?</legend>
-        {isQuantitySelected && !isCapsuleSelected && (
-          <div>
-            <div>
-              <input
-                className={className.input}
-                {...register("grindOption")}
-                type="radio"
-                id="grindOption-wholebean"
-                value="Wholebean"
-              />
-              <label
-                className={className.label}
-                htmlFor="grindOption-wholebean"
-              >
-                <span className={className["label-span"]}>Wholebean</span> Best
-                choice if you cherish the full sensory experience
-              </label>
-            </div>
-
-            <div>
-              <input
-                className={className.input}
-                {...register("grindOption")}
-                type="radio"
-                id="grindOption-filter"
-                value="Filter"
-              />
-              <label className={className.label} htmlFor="grindOption-filter">
-                <span className={className["label-span"]}>Filter</span> For drip
-                or pour-over coffee methods such as V60 or Aeropress
-              </label>
-            </div>
-
-            <div>
-              <input
-                className={className.input}
-                {...register("grindOption")}
-                type="radio"
-                id="grindOption-cafetiere"
-                value="Cafetiére"
-              />
-              <label
-                className={className.label}
-                htmlFor="grindOption-cafetiere"
-              >
-                <span className={className["label-span"]}>Cafetiére</span>{" "}
-                Course ground beans specially suited for french press coffee
-              </label>
-            </div>
-          </div>
-        )}
-      </fieldset>
-
-      <fieldset
-        className={className.fieldset}
-        aria-expanded={isGrindOptionSelected}
-        aria-live="polite"
-      >
-        <legend>How often should we deliver?</legend>
-        {isEverythingForDeliveriesSelected && (
-          <div>
-            <div>
-              <input
-                className={className.input}
-                {...register("deliveries", { required: true })}
-                type="radio"
-                id="deliveries-everyWeek"
-                value="Every week"
-              />
-              <label className={className.label} htmlFor="deliveries-everyWeek">
-                <span className={className["label-span"]}>Every week</span> $
-                {prices[quantity].everyWeek} per shipment. Includes free
-                first-class shipping.
-              </label>
-            </div>
-
-            <div>
-              <input
-                className={className.input}
-                {...register("deliveries", { required: true })}
-                type="radio"
-                id="deliveries-everyTwoWeeks"
-                value="Every two weeks"
-              />
-              <label
-                className={className.label}
-                htmlFor="deliveries-everyTwoWeeks"
-              >
-                <span className={className["label-span"]}>Every two weeks</span>{" "}
-                ${prices[quantity].everyTwoWeeks} per shipment. Includes free
-                priority shipping.
-              </label>
-            </div>
-
-            <div>
-              <input
-                className={className.input}
-                {...register("deliveries", { required: true })}
-                type="radio"
-                id="deliveries-everyMonth"
-                value="Every month"
-              />
-              <label
-                className={className.label}
-                htmlFor="deliveries-everyMonth"
-              >
-                <span className={className["label-span"]}>Every month</span> $
-                {prices[quantity].everyMonth} per shipment. Includes free
-                priority shipping.
-              </label>
-            </div>
-          </div>
-        )}
-      </fieldset>
+                        <p
+                          className=""
+                          id={`${questionSplit}-${optionSplit}-d`}
+                        >
+                          {isQuestionFive
+                            ? // @ts-expect-error - TS doesn't like the optional chaining here
+                              `${currentFormStep === 5 ? `$${prices[formData["Quantity"]]?.[optionSplit]} per shipment. ` : ""}`
+                            : ""}
+                          {option.description}
+                        </p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </fieldset>
+              {errors[questionSplit] && (
+                <p>{errors[questionSplit]?.message as string}</p>
+              )}
+            </li>
+          );
+        })}
+      </ul>
 
       <div className="mt-4">
         <h2>Order summary</h2>
-        <p>
-          {/* "I drink my coffee{" "}
-          {isCapsuleSelected
-            ? "using Capsules"
-            : `as ${watch("preferences") || "_____"}`}
-          , with a {watch("beanType") || "_____"} type of bean.{" "}
-          {watch("quantity")}
-          {watch("preferences") !== "Capsule"
-            ? watch("grindOption")
-              ? ` ground ala ${watch("grindOption")}`
-              : " ground ala _____"
-            : ""}
-          , sent to me {watch("deliveries") || "_____"}." */}
-          {summaryString}
-        </p>
+        <p>{summaryString}</p>
       </div>
 
-      <button className="mt-4 p-2" disabled={isSubmitting}>
+      <Btn
+        className="mt-4 p-2"
+        isDisabled={isSubmitting || currentFormStep < 6}
+      >
         {isSubmitting ? "Loading..." : "Create my plan!"}
-      </button>
+      </Btn>
     </form>
   );
 };
