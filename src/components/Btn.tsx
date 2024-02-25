8fa96ae645680bea5fd77bc61ad3c6066a2af4cc
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-02-25 23:59:24 +0100

    added disabled state

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -4,10 +4,11 @@ interface BtnProps {
   className?: string;
 }
 
-const Btn = ({ children, className }: BtnProps) => {
+const Btn = ({ children, className, isDisabled }: BtnProps) => {
   return (
     <button
-      className={`text-md rounded-lg bg-custom-dark-cyan px-8 py-4 font-fraunces font-black leading-6 text-custom-light-cream ${className ? className : ""}`}
+      disabled={isDisabled}
+      className={`rounded-lg bg-custom-dark-cyan px-8 py-4 font-fraunces text-md font-black leading-6 text-custom-light-cream disabled:bg-custom-dark-grey ${className ? className : ""}`}
     >
       {children}
     </button>
