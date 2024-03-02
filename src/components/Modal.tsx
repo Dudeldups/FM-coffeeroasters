02e307b7d32d713e032e5eb1cafddc5c16842a3f
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-03-02 19:50:57 +0100

    added md styles

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,10 +13,10 @@ const Modal = ({ formData, setIsModalOpen, totalPrice }: ModalProps) => {
   return (
     <div className="fixed inset-0 z-40 grid place-content-center bg-black bg-opacity-80 p-4">
       <div className="overflow-hidden rounded-lg">
-        <div className="z-50 bg-custom-dark-blue p-6">
-          <h2 className="text-custom-light-cream">Order Summary</h2>
+        <div className="z-50 bg-custom-dark-blue p-3 xs:p-6 md:p-12">
+          <h2 className="text-custom-light-cream md:text-2xl">Order Summary</h2>
         </div>
-        <div className="z-50 bg-custom-light-cream p-6 pb-10">
+        <div className="z-50 grid max-w-96 bg-custom-light-cream p-3 pb-10 xs:p-6 md:max-w-[33.75rem] md:p-12">
           <Summary
             formData={formData}
             className="mb-3 text-lg text-custom-dark-grey"
@@ -26,9 +26,18 @@ const Modal = ({ formData, setIsModalOpen, totalPrice }: ModalProps) => {
             selection if something is off. Subscription discount codes can also
             be redeemed at the checkout.
           </p>
-          <Btn onClick={() => setIsModalOpen(false)} className="mt-6">
-            Checkout <span>{" - $" + totalPrice}</span>
-          </Btn>
+          <div className="mt-6 gap-2 md:grid md:grid-cols-2 md:items-center">
+            <p className="font-fraunces text-xl font-black text-black  mobile:hidden">
+              {`$ ${totalPrice} / mo`}
+            </p>
+            <Btn
+              onClick={() => setIsModalOpen(false)}
+              className="px-2 mobile:w-full"
+            >
+              Checkout{" "}
+              <span className="md:hidden">{` - $ ${totalPrice} / mo`}</span>
+            </Btn>
+          </div>
         </div>
       </div>
     </div>
